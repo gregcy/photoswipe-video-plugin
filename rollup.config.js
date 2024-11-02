808699c8555f399534b89a7d15b03d3a105252e2
Greg Andreou <greg.andreou@gmail.com>
2024-11-02 10:32:05 +0200

    Added UMD output settings to rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,11 @@
 import { terser } from "rollup-plugin-terser";
- 
+
 export default {
   input: "src/index.js",
   output: [
     { file: "dist/photoswipe-video-plugin.esm.js", format: "esm" },
     { file: "dist/photoswipe-video-plugin.esm.min.js", format: "esm", plugins: [ terser()] },
+    { file: "dist/photoswipe-video-plugin.umd.js", format: "umd", name: "PhotoSwipeVideoPlugin" },
+    { file: "dist/photoswipe-video-plugin.umd.min.js", format: "umd", name: "PhotoSwipeVideoPlugin", plugins: [ terser()] },
   ],
 };
